4ea41d98015e0765a84f6e43047fd2549c412df9
fernandovincent <s21810081@student.unklab.ac.id>
2021-03-10 19:22:39 +0800

    Version Control (Git Basic)

---
+++ latihan-git/index.js
@@ -0,0 +1,16 @@
+// Senin
+console.log("Baris Pertama");
+
+// Selasa 
+console.log("Baris Kedua");
+console.log("Baris Ketiga");
+
+// Rabu
+console.log("Baris Keempat");
+console.log("Baris Kelima");
+
+// Kamis
+console.log("Baris Keenam");
+
+// Jumat
+console.log("Baris Ketujuh");
